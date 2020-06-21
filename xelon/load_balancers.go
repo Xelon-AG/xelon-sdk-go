3d14c8b4ad2d3fc45cd464af53aaeaa36cd661bf
pavel <36902936+pavel-github@users.noreply.github.com>
2020-06-21 05:02:08 +0200

    Add methods for load balancer API endpoint
    - Create() - create a new load balancer
    - Delete() - delete a load balancer

--- xelon/load_balancers.go
+++ xelon/load_balancers.go
@@ -19,6 +19,16 @@ type LoadBalancer struct {
 	Type    int    `json:"type,omitempty"`
 }
 
+type LoadBalancerCreateRequest struct {
+	Name     string   `json:"name,omitempty"`
+	ServerID []string `json:"server_id,omitempty"`
+	Type     int      `json:"type,omitempty"`
+}
+
+type LoadBalancerCreateResponse struct {
+	Message string `json:"message,omitempty"`
+}
+
 // List provides information about load balancers.
 func (s *LoadBalancerService) List(ctx context.Context, tenantID string) ([]LoadBalancer, *http.Response, error) {
 	if tenantID == "" {
@@ -59,4 +69,43 @@ func (s *LoadBalancerService) Get(ctx context.Context, tenantID, localID string)
 	}
 
 	return loadBalancer, resp, nil
+}
+
+// Create makes a new load balancer with given payload.
+func (s *LoadBalancerService) Create(ctx context.Context, tenantID string, loadBalancerCreateRequest *LoadBalancerCreateRequest) (*LoadBalancerCreateResponse, *http.Response, error) {
+	if tenantID == "" {
+		return nil, nil, ErrEmptyArgument
+	}
+	if loadBalancerCreateRequest == nil {
+		return nil, nil, ErrEmptyPayloadNotAllowed
+	}
+
+	path := fmt.Sprintf("%v/%v", tenantID, loadBalancerBasePath)
+	req, err := s.client.NewRequest(http.MethodPost, path, loadBalancerCreateRequest)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	loadBalancerCreateResponse := new(LoadBalancerCreateResponse)
+	resp, err := s.client.Do(ctx, req, loadBalancerCreateResponse)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return loadBalancerCreateResponse, resp, nil
+}
+
+// Delete removes a load balancer.
+func (s *LoadBalancerService) Delete(ctx context.Context, tenantID, localID string) (*http.Response, error) {
+	if tenantID == "" || localID == "" {
+		return nil, ErrEmptyArgument
+	}
+
+	path := fmt.Sprintf("%v/%v/%v", tenantID, loadBalancerBasePath, localID)
+	req, err := s.client.NewRequest(http.MethodDelete, path, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return s.client.Do(ctx, req, nil)
 }
