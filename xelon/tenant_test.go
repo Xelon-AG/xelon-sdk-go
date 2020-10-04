6cd0bb09c7317b6368430ee13d633470b233ac6d
pavel <36902936+pavel-github@users.noreply.github.com>
2020-10-04 04:59:00 +0200

    Add unit tests for 'tenant' service

---
+++ xelon/tenant_test.go
@@ -0,0 +1,27 @@
+package xelon
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTenant_Get(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/tenant/", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodGet, r.Method)
+		_, _ = fmt.Fprint(w, `{"tenant_identifier":"long-id"}`)
+	})
+	expected := &Tenant{
+		TenantID: "long-id",
+	}
+
+	tenant, _, err := client.Tenant.Get(ctx)
+
+	assert.NoError(t, err)
+	assert.Equal(t, expected, tenant)
+}
