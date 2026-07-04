5dfccd2caaf235b21c984be3629a7c203804b784
pavel <36902936+pavel-github@users.noreply.github.com>
2026-07-05 01:32:56 +0200

    feat(object-storages): add method to manage buckets

--- xelon/object_storages.go
+++ xelon/object_storages.go
@@ -16,14 +16,14 @@ type ObjectStoragesService service
 
 // ObjectStorageUser represents a Xelon users for S3-compatible object storage.
 type ObjectStorageUser struct {
-	ID                     string                   `json:"identifier,omitempty"`
-	Name                   string                   `json:"name,omitempty"`
-	QuotaGB                int                      `json:"quota,omitempty"`
-	S3Endpoints            []string                 `json:"s3endpoints,omitempty"`
-	Tenant                 *Tenant                  `json:"tenant,omitempty"`
-	Tokens                 []ObjectStorageUserToken `json:"tokens,omitempty"`
-	UsedGB                 float32                  `json:"sizeUsedGb,omitempty"`
-	ZoneReplicationEnabled bool                     `json:"isReplicated,omitempty"`
+	ID                       string                   `json:"identifier,omitempty"`
+	Name                     string                   `json:"name,omitempty"`
+	QuotaGB                  int                      `json:"quota,omitempty"`
+	RegionReplicationEnabled bool                     `json:"isReplicated,omitempty"`
+	S3Endpoints              []string                 `json:"s3endpoints,omitempty"`
+	Tenant                   *Tenant                  `json:"tenant,omitempty"`
+	Tokens                   []ObjectStorageUserToken `json:"tokens,omitempty"`
+	UsedGB                   float32                  `json:"sizeUsedGb,omitempty"`
 }
 
 type ObjectStorageUserCreateRequest struct {
@@ -232,6 +232,258 @@ func (s *ObjectStoragesService) DeleteUserToken(ctx context.Context, objectStora
 	return s.client.Do(ctx, req, nil)
 }
 
+type ObjectStorageBucket struct {
+	CreatedAt                *time.Time `json:"createdAt,omitempty"`
+	ID                       string     `json:"identifier"`
+	IPRestrictionsEnabled    bool       `json:"isWithRestrictedIps,omitempty"`
+	Name                     string     `json:"name,omitempty"`
+	ObjectLockEnabled        bool       `json:"isObjectLock,omitempty"`
+	ObjectStorageUserID      string     `json:"s3UserIdentifier,omitempty"`
+	ObjectStorageUserName    string     `json:"s3UserName,omitempty"`
+	RegionName               string     `json:"zoneGroup,omitempty"`
+	RegionReplicationEnabled bool       `json:"isReplicated,omitempty"`
+	S3Endpoints              []string   `json:"s3endpoints,omitempty"`
+	Tenant                   *Tenant    `json:"tenant,omitempty"`
+	VersioningEnabled        bool       `json:"isVersioning,omitempty"`
+}
+
+type ObjectStorageBucketCreateRequest struct {
+	Name                    string `json:"name"`
+	ObjectLockEnabled       bool   `json:"isObjectLock"`
+	ObjectLockRetentionDays int    `json:"retentionPeriodDays,omitempty"`
+	ObjectStorageUserID     string `json:"s3UserIdentifier"`
+	VersioningEnabled       bool   `json:"isVersioning"`
+}
+
+type ObjectStorageBucketUpdateRequest struct {
+	Name string `json:"name"`
+}
+
+// ObjectStorageBucketListOptions specifies the optional parameters to the ObjectStoragesService.ListBuckets.
+type ObjectStorageBucketListOptions struct {
+	TenantID string `url:"tenantId,omitempty"`
+
+	ListOptions
+}
+
+type ObjectStorageBucketVersioningUpdateRequest struct {
+	VersioningEnabled bool `json:"isVersioning"`
+}
+
+type ObjectStorageBucketIPRestrictions struct {
+	AllowedIPs []string `json:"allowedIps,omitempty"`
+	Enabled    bool     `json:"enabled,omitempty"`
+}
+
+type ObjectStorageBucketIPRestrictionsUpdateRequest struct {
+	AllowedIPs []string `json:"allowedIps,omitempty"`
+	Enabled    bool     `json:"enabled"`
+}
+
+type objectStorageBucketRoot struct {
+	ObjectStorageBucket *ObjectStorageBucket `json:"data,omitempty"`
+}
+
+type objectStorageBucketsRoot struct {
+	ObjectStorageBuckets []ObjectStorageBucket `json:"data"`
+	Meta                 *Meta                 `json:"meta,omitempty"`
+}
+
+type objectStorageBucketIPRestrictionsRoot struct {
+	ObjectStorageBucketIPRestrictions *ObjectStorageBucketIPRestrictions `json:"data,omitempty"`
+}
+
+func (v ObjectStorageBucket) String() string { return Stringify(v) }
+
+func (v ObjectStorageBucketIPRestrictions) String() string { return Stringify(v) }
+
+// ListBuckets provides a list of available object storage buckets.
+func (s *ObjectStoragesService) ListBuckets(ctx context.Context, opts *ObjectStorageBucketListOptions) ([]ObjectStorageBucket, *Response, error) {
+	path, err := addOptions(fmt.Sprintf("%v/buckets", objectStorageBasePath), opts)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	req, err := s.client.NewRequest(http.MethodGet, path, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	root := new(objectStorageBucketsRoot)
+	resp, err := s.client.Do(ctx, req, root)
+	if err != nil {
+		return nil, resp, err
+	}
+	if m := root.Meta; m != nil {
+		resp.Meta = m
+	}
+
+	return root.ObjectStorageBuckets, resp, nil
+}
+
+// AllBuckets returns an iterator to paginate over all object storage buckets.
+//
+// The return iterator can be used in a for...range loop to easily process all buckets.
+func (s *ObjectStoragesService) AllBuckets(ctx context.Context, opts *ListOptions) (iter.Seq2[ObjectStorageBucket, *Response], func() error) {
+	return newPaginator[ObjectStorageBucket](ctx, s.client, fmt.Sprintf("%v/buckets", objectStorageBasePath), opts)
+}
+
+// GetBucket provides detailed information for object storage bucket identified by name and user id.
+func (s *ObjectStoragesService) GetBucket(ctx context.Context, bucketName, objectStorageUserID string) (*ObjectStorageBucket, *Response, error) {
+	if bucketName == "" {
+		return nil, nil, fmt.Errorf("bucket name: %w", ErrEmptyArgument)
+	}
+	if objectStorageUserID == "" {
+		return nil, nil, fmt.Errorf("object storage user id: %w", ErrEmptyArgument)
+	}
+
+	path := fmt.Sprintf("%v/buckets/%v/%v", objectStorageBasePath, bucketName, objectStorageUserID)
+	req, err := s.client.NewRequest(http.MethodGet, path, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	bucket := new(ObjectStorageBucket)
+	resp, err := s.client.Do(ctx, req, bucket)
+	if err != nil {
+		return nil, resp, err
+	}
+	if bucket.ID == "" && bucket.Name == "" {
+		return nil, resp, errors.New("object storage bucket data is empty")
+	}
+
+	return bucket, resp, nil
+}
+
+// CreateBucket makes a new object storage bucket with given payload.
+func (s *ObjectStoragesService) CreateBucket(ctx context.Context, createRequest *ObjectStorageBucketCreateRequest) (*ObjectStorageBucket, *Response, error) {
+	if createRequest == nil {
+		return nil, nil, fmt.Errorf("payload: %w", ErrEmptyPayloadNotAllowed)
+	}
+
+	path := fmt.Sprintf("%v/buckets", objectStorageBasePath)
+	req, err := s.client.NewRequest(http.MethodPost, path, createRequest)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	root := new(objectStorageBucketRoot)
+	resp, err := s.client.Do(ctx, req, root)
+	if err != nil {
+		return nil, resp, err
+	}
+	if root.ObjectStorageBucket == nil {
+		return nil, resp, errors.New("object storage bucket data is empty")
+	}
+
+	return root.ObjectStorageBucket, resp, nil
+}
+
+// UpdateBucket changes an object storage bucket identified by id.
+func (s *ObjectStoragesService) UpdateBucket(ctx context.Context, bucketID string, updateRequest *ObjectStorageBucketUpdateRequest) (*Response, error) {
+	if bucketID == "" {
+		return nil, fmt.Errorf("bucket id: %w", ErrEmptyArgument)
+	}
+	if updateRequest == nil {
+		return nil, fmt.Errorf("payload: %w", ErrEmptyPayloadNotAllowed)
+	}
+
+	path := fmt.Sprintf("%v/buckets/%v", objectStorageBasePath, bucketID)
+	req, err := s.client.NewRequest(http.MethodPut, path, updateRequest)
+	if err != nil {
+		return nil, err
+	}
+
+	return s.client.Do(ctx, req, nil)
+}
+
+// DeleteBucket removes object storage bucket identified by name and user id.
+func (s *ObjectStoragesService) DeleteBucket(ctx context.Context, bucketName, objectStorageUserID string) (*Response, error) {
+	if bucketName == "" {
+		return nil, fmt.Errorf("bucket name: %w", ErrEmptyArgument)
+	}
+	if objectStorageUserID == "" {
+		return nil, fmt.Errorf("object storage user id: %w", ErrEmptyArgument)
+	}
+
+	path := fmt.Sprintf("%v/buckets/%v/%v", objectStorageBasePath, bucketName, objectStorageUserID)
+	req, err := s.client.NewRequest(http.MethodDelete, path, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return s.client.Do(ctx, req, nil)
+}
+
+// UpdateBucketVersioning changes versioning for an object storage bucket.
+func (s *ObjectStoragesService) UpdateBucketVersioning(ctx context.Context, bucketName, objectStorageUserID string, updateRequest *ObjectStorageBucketVersioningUpdateRequest) (*Response, error) {
+	if bucketName == "" {
+		return nil, fmt.Errorf("bucket name: %w", ErrEmptyArgument)
+	}
+	if objectStorageUserID == "" {
+		return nil, fmt.Errorf("object storage user id: %w", ErrEmptyArgument)
+	}
+	if updateRequest == nil {
+		return nil, fmt.Errorf("payload: %w", ErrEmptyPayloadNotAllowed)
+	}
+
+	path := fmt.Sprintf("%v/buckets/%v/%v/versioning", objectStorageBasePath, bucketName, objectStorageUserID)
+	req, err := s.client.NewRequest(http.MethodPut, path, updateRequest)
+	if err != nil {
+		return nil, err
+	}
+
+	return s.client.Do(ctx, req, nil)
+}
+
+// GetBucketIPRestrictions provides IP restriction settings for an object storage bucket.
+func (s *ObjectStoragesService) GetBucketIPRestrictions(ctx context.Context, bucketName, objectStorageUserID string) (*ObjectStorageBucketIPRestrictions, *Response, error) {
+	if bucketName == "" {
+		return nil, nil, fmt.Errorf("bucket name: %w", ErrEmptyArgument)
+	}
+	if objectStorageUserID == "" {
+		return nil, nil, fmt.Errorf("object storage user id: %w", ErrEmptyArgument)
+	}
+
+	path := fmt.Sprintf("%v/buckets/%v/%v/ip-restrictions", objectStorageBasePath, bucketName, objectStorageUserID)
+	req, err := s.client.NewRequest(http.MethodGet, path, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	root := new(objectStorageBucketIPRestrictionsRoot)
+	resp, err := s.client.Do(ctx, req, root)
+	if err != nil {
+		return nil, resp, err
+	}
+	if root.ObjectStorageBucketIPRestrictions == nil {
+		return nil, resp, errors.New("object storage bucket ip restrictions data is empty")
+	}
+
+	return root.ObjectStorageBucketIPRestrictions, resp, nil
+}
+
+// UpdateBucketIPRestrictions changes IP restriction settings for an object storage bucket.
+func (s *ObjectStoragesService) UpdateBucketIPRestrictions(ctx context.Context, bucketName, objectStorageUserID string, updateRequest *ObjectStorageBucketIPRestrictionsUpdateRequest) (*Response, error) {
+	if bucketName == "" {
+		return nil, fmt.Errorf("bucket name: %w", ErrEmptyArgument)
+	}
+	if objectStorageUserID == "" {
+		return nil, fmt.Errorf("object storage user id: %w", ErrEmptyArgument)
+	}
+	if updateRequest == nil {
+		return nil, fmt.Errorf("payload: %w", ErrEmptyPayloadNotAllowed)
+	}
+
+	path := fmt.Sprintf("%v/buckets/%v/%v/ip-restrictions", objectStorageBasePath, bucketName, objectStorageUserID)
+	req, err := s.client.NewRequest(http.MethodPut, path, updateRequest)
+	if err != nil {
+		return nil, err
+	}
+
+	return s.client.Do(ctx, req, nil)
+}
+
 // ObjectStoragePlan represents a Xelon object storage pricing plan.
 type ObjectStoragePlan struct {
 	Description          string  `json:"description,omitempty"`
@@ -260,11 +512,11 @@ func (s *ObjectStoragesService) ListPlans(ctx context.Context) ([]ObjectStorageP
 	return plans, resp, nil
 }
 
-// ObjectStorageRegion represents a Xelon object storage regions (zone group).
+// ObjectStorageRegion represents a Xelon object storage region.
 type ObjectStorageRegion struct {
-	ID                 string `json:"id"`
-	Name               string `json:"name"`
-	ReplicationEnabled bool   `json:"hasReplication"`
+	RegionID                 string `json:"id"`
+	RegionName               string `json:"name"`
+	RegionReplicationEnabled bool   `json:"hasReplication"`
 }
 
 func (v ObjectStorageRegion) String() string { return Stringify(v) }
